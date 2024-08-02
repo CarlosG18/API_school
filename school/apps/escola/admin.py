25081b197886a4de3f1b4da43f69acf96403debe
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-08-02 16:21:51 -0300

    :sparkles: feat: registering models estudantes and curso on admin site

--- school/apps/escola/admin.py
+++ school/apps/escola/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Estudante, Curso
 
-# Register your models here.
+@admin.register(Estudante)
+class EstudanteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'nascimento', 'telefone')
+    list_display_links = ('nome',)
+    search_fields = ('nome',)
+    list_per_page = 10
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao', 'nivel')
+    search_fields = ('codigo',)
--- school/apps/escola/models.py
+++ school/apps/escola/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Estudantes(models.Model):
+class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=50, blank=False)
     cpf = models.CharField(max_length=11)
