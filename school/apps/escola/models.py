e564823e2b8ce7a1ed2e854fae4f4ece6e53e9f3
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-08-02 16:08:25 -0300

    :sparkles: feat: creating models estudante e curso

--- school/apps/escola/models.py
+++ school/apps/escola/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Estudantes(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50, blank=False)
+    cpf = models.CharField(max_length=11)
+    nascimento = models.DateField()
+    telefone = models.CharField(max_length=14)
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL_CHOICES = (
+        ('B','Básico'),
+        ('I','Intermediário'),
+        ('A','Avançado'),
+    )
+
+    codigo = models.CharField(max_length=10)
+    descricao = models.TextField()
+    nivel = models.CharField(max_length=1, choices=NIVEL_CHOICES,blank=False, null=False, default='B')
+
+    def __str__(self) -> str:
+        return self.codigo
