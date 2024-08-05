f8d4ff4beca5ef32e83b935ee3d40c56ba05f9df
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-08-05 18:48:56 -0300

    :sparkles: feat: creating routers for estudantes and cursos

--- school/apps/escola/views.py
+++ school/apps/escola/views.py
@@ -1,5 +1,5 @@
-from escola.models import Estudante, Curso
-from escola.serializers import EstudanteSerializer, CursoSerializer
+from .models import Estudante, Curso
+from .serializers import EstudanteSerializer, CursoSerializer
 from rest_framework import viewsets
 
 class EstudanteViewSet(viewsets.ModelViewSet):
@@ -10,4 +10,3 @@ class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
-    
--- school/setup/urls.py
+++ school/setup/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.escola.views import EstudanteViewSet, CursoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('estudantes', EstudanteViewSet, basename="Estudantes")
+router.register('cursos', CursoViewSet, basename="Cursos")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('escola/', include('apps.escola.urls')),
+    path('', include(router.urls)),
 ]
